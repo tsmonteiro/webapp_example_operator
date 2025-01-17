62f3edc1e5d2ecba6dba4501089dff8647a16ba5
Thiago <tsmonteiro@gmail.com>
2025-01-17 09:26:09 -0300

    upload data

--- lib/screens/upload_data_screen.dart
+++ lib/screens/upload_data_screen.dart
@@ -6,6 +6,8 @@ import 'package:webapp_model/webapp_data_base.dart';
 import 'package:webapp_template/webapp_data.dart';
 import 'package:webapp_ui_commons/mixin/progress_log.dart';
 
+import 'package:webapp_components/components/upload_multi_file_component.dart';
+
 class UploadDataScreen extends StatefulWidget {
   final WebAppData modelLayer;
   const UploadDataScreen(this.modelLayer, {super.key});
@@ -35,6 +37,12 @@ class _UploadDataScreenState extends State<UploadDataScreen>
   @override
   void initState() {
     super.initState();
+
+    var uploadComponent = UploadFileComponent("uploadComp", getScreenId(), "Upload Files", 
+          modelLayer.app.projectId, modelLayer.app.teamname);
+
+
+    addComponent("default", uploadComponent);
 
     initScreen(widget.modelLayer as WebAppDataBase);
   }
